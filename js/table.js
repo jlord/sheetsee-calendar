a0416f7b926f2027027d1a7d9eb3c7d2b604c111
Jessica Lord <jessica.lord@github.com>
2014-07-17 12:55:50 -0700

    Revert "merge updates"
    This reverts commit 2425782db0be7674cbf06fcb55ba0fa2ddfebb4b, reversing
    changes made to f54228780c23c888ee4a8042ca11f47366723293.

--- js/table.js
+++ js/table.js
@@ -1,12 +1,11 @@
-var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
-var weekdays   = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-var today  = new Date()
-var months = []
-
 function generateCalendar (eventData) {
+  monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+  weekdays   = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+  today      = new Date()
+  months = []
   generateAllTheMonths(eventData)
 
-  eventData.forEach(function (event) {
+  $.each(eventData, function(i, event){
     appendEvent(event)
   })
 
@@ -29,38 +28,32 @@ function addMonthMenu() {
     e.preventDefault()
   })
 
-  // Get current month and click it
-  var currentMonth = $('[data-target=' + monthNames[(new Date()).getMonth()] + ']')
-  if( currentMonth.length ) {
-    currentMonth.click()
-  } else {
-    $('[data-target]').first().click()
-  }
+  $('[data-target]').first().click()
 }
 
 function appendEvent( event ) {
-  var eventStartDate = new Date(event.startdate)
-  var eventEndDate   = new Date(event.enddate)
-  var eventElement   = $('<div class="event"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>')
+  eventStartDate = new Date(event.startdate)
+  eventEndDate   = new Date(event.enddate)
+  eventElement   = $('<div class="event"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>')
 
   // Handle multi-days
-  if ( eventEndDate.getDate() ) {
-    var date         = eventStartDate
-    var spacerNumber = $('#' + formattedDate(eventStartDate)).find('.event').length
+  if( eventEndDate.getDate() ) {
+    date         = eventStartDate
+    spacerNumber = $('#' + formattedDate(eventStartDate)).find('.event').length
     eventElement.addClass('multi-days')
 
-    while ( eventEndDate > date ) {
+    while( eventEndDate > date ) {
       // If reached end of month, go to first day of the next month
       // Else go to the next day
-      if (date == new Date(date.getFullYear(), date.getMonth() + 1, 0)) {
+      if(date == new Date(date.getFullYear(), date.getMonth() + 1, 0)) {
         date == new Date(date.getFullYear(), date.getMonth() + 1, 1)
       } else {
         date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1 )
       }
 
       // Add spacer to line up the event
-      var dateElement = $('#' + formattedDate(date))
-      var steps = dateElement.find('.event').length
+      dateElement = $('#' + formattedDate(date))
+      steps = dateElement.find('.event').length
       loopForTimes( spacerNumber - steps, function() {
         dateElement.append('<div class="event spacer">&nbsp;</div>')
       })
@@ -73,17 +66,12 @@ function appendEvent( event ) {
 }
 
 function generateAllTheMonths( eventData ) {
-  var dates = []
-  var months = []
-
-  eventData.forEach(function(event) {
-    if (event.startdate) dates.push(event.startdate)
-    if (event.enddate) dates.push(event.enddate)
-  })
-
-  dates.forEach(function (date) {
+  dates  = eventData.map(function(e) { return [e.startdate, e.enddate] })
+  dates  = [].concat.apply([], dates).filter(function(n) { return n })
+  months = []
+  $.each( dates, function(_, date) {
     date = new Date(date)
-    if(months.indexOf(date.getMonth()) < 0) {
+    if( months.indexOf(date.getMonth()) < 0 ) {
       months.push(date.getMonth())
       generateMonthTable(date)
     }
@@ -91,48 +79,45 @@ function generateAllTheMonths( eventData ) {
 }
 
 function generateMonthTable( date ) {
-  var eventMonthName = monthNames[date.getMonth()]
-  var monthTable     = $('<table cellspacing=0 class="month-table" data-month="' + eventMonthName + '" id="month-' + date.getMonth() + '"></table>')
-  var monthTableBody = monthTable.append('<tbody>')
-  // var today          = new Date()
-  var endOfToday     = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 00, 00, 00)
-  var firstDay       = new Date(date.getFullYear(), date.getMonth(), 1)
-  var numberOfDays   = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
-  var weekDayNumber  = firstDay.getDay()
+  eventMonthName = monthNames[date.getMonth()]
+  monthTable     = $('<table cellspacing=0 class="month-table" data-month="' + eventMonthName + '" id="month-' + date.getMonth() + '"></table>')
+  monthTableBody = monthTable.append('<tbody>')
+  firstDay       = new Date(date.getFullYear(), date.getMonth(), 1)
+  numberOfDays   = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+  weekDayNumber  = firstDay.getDay()
 
   $('#calendar-goes-here').append(monthTable)
   monthTable.before('<h2 data-month="' + eventMonthName + '">' + eventMonthName + ' ' + date.getFullYear() + '</h2>')
 
   // Add month calendar header
   monthTableBody.append('<tr class="header"></tr>')
-  var headerRow = monthTableBody.find('.header')
+  headerRow = monthTableBody.find('.header')
   loopForTimes( 7, function(i) {
     headerRow.append('<td>' + weekdays[i] + '</td>')
   })
 
   // Add empty days from previous month
-  var times = weekDayNumber == 0 ? 6 : weekDayNumber - 1
-  loopForTimes( times, function() {
+  loopForTimes( weekDayNumber - 1, function() {
     getFirstAvailableRow(monthTable).append('<td class="empty"></td>')
   })
 
   // Filling the month with days
   loopForTimes( numberOfDays, function(daynumber) {
-    var thisDay = new Date(date.getFullYear(), date.getMonth(), (daynumber + 1))
-    var id = formattedDate(thisDay)
-    var pastClass = endOfToday > thisDay ? "past" : ""
-    getFirstAvailableRow(monthTableBody).append('<td class="no-event ' + pastClass + '" id=' + id + '><div class=day>'+ (daynumber + 1) +'</div></td>')
+    thisDay = new Date(date.getFullYear(), date.getMonth(), (daynumber + 1))
+    id = formattedDate(thisDay)
+    getFirstAvailableRow(monthTableBody).append('<td class="no-event" id=' + id + '><div class=day>'+ (daynumber + 1) +'</div></td>')
   })
 
   // Add empty days from next month
-  var lastRow = monthTable.find('tr:last')
-  var cellsInLastRow = lastRow.find('td').length
+  lastRow = monthTable.find('tr:last')
+  cellsInLastRow = lastRow.find('td').length
   // Check if this is necessary
-  if ( cellsInLastRow < 7 ) {
+  if( cellsInLastRow < 7 ) {
     loopForTimes( (7 - cellsInLastRow), function() {
       lastRow.append('<td class="empty"></td>')
     })
   }
+
 }
 
 // Because I don't like ot write for()
@@ -144,13 +129,13 @@ function loopForTimes( times, callback ) {
 
 // This is handy: getting the first row with available cell space
 function getFirstAvailableRow( table ) {
-  var row = table.find('tr.days').filter(function(i, thisRow) {
+  row = table.find('tr.days').filter(function(i, thisRow) {
     return ($(thisRow).find('td').length) < 7
   })
   // If no available row, create a new one
   if( row.length == 0 ) {
     table.append('<tr class=days>')
-    var row = table.find('tr').last()
+    row = table.find('tr').last()
   }
   return row
 }
--- js/tabletop.js
+++ js/tabletop.js
@@ -96,8 +96,9 @@
     }
 
     if(/pubhtml/.test(this.key)) {
-      this.log("You passed a new Google Spreadsheets url as the key! Attempting to parse.");
+      alert("You passed a new Google Spreadsheets url as the key! This won't work yet, you'll need to change back to old Sheets.");
       this.key = this.key.match("d\\/(.*?)\\/pubhtml")[1];
+      console.log(this.key);
     }
 
     if(!this.key) {
@@ -308,16 +309,12 @@
         this.foundSheetNames.push(data.feed.entry[i].title.$t);
         // Only pull in desired sheets to reduce loading
         if( this.isWanted(data.feed.entry[i].content.$t) ) {
-          var linkIdx = data.feed.entry[i].link.length-1;
-          var sheet_id = data.feed.entry[i].link[linkIdx].href.split('/').pop();
-          var json_path = "/feeds/list/" + this.key + "/" + sheet_id + "/public/values?alt="
+          var sheet_id = data.feed.entry[i].link[3].href.substr( data.feed.entry[i].link[3].href.length - 3, 3);
+          var json_path = "/feeds/list/" + this.key + "/" + sheet_id + "/public/values?sq=" + this.query + '&alt='
           if (inNodeJS || supportsCORS) {
             json_path += 'json';
           } else {
             json_path += 'json-in-script';
-          }
-          if(this.query) {
-            json_path += "&sq=" + this.query;
           }
           if(this.orderby) {
             json_path += "&orderby=column:" + this.orderby.toLowerCase();
